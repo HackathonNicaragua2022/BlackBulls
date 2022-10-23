9ae2063dccd0c7639f45f670463a3b74edf58d68
Waskar <waskareliasrios11@gmail.com>
2022-10-23 00:39:27 -0600

    pantalla de informacion

---
+++ app/src/main/java/com/example/trazabilidad/perfila.kt
@@ -0,0 +1,11 @@
+package com.example.trazabilidad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class perfila : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_perfila)
+    }
+}
