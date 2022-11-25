b3a45d565e88425756a48969eb96e0d84d586229
Waskar <waskareliasrios11@gmail.com>
2022-11-25 13:01:25 -0600

    comomentarios de la programacion

--- app/src/main/java/com/example/trazabilidad/hvacunas.kt
+++ app/src/main/java/com/example/trazabilidad/hvacunas.kt
@@ -3,7 +3,7 @@ package com.example.trazabilidad
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class hcvacunas : AppCompatActivity() {
+class hvacunas : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hcvacunas)
