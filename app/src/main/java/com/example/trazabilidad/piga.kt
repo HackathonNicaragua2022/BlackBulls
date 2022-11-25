b003065cba9c0b481adc3f857438d044f5e2e8b9
BrayanMontenegro <montenegrobrayan45@gmail.com>
2022-11-25 14:30:36 -0600

    R-1

---
+++ app/src/main/java/com/example/trazabilidad/piga.kt
@@ -0,0 +1,11 @@
+package com.example.trazabilidad
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class piga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_piga)
+    }
+}
