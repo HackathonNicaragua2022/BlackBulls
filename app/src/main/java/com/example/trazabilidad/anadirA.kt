34020692d051e09d80df2aed6ad38757fdc7cddf
Waskar <waskareliasrios11@gmail.com>
2022-11-25 10:24:42 -0600

    restructuracion del proyecto

--- app/src/main/java/com/example/trazabilidad/anadirA.kt
+++ app/src/main/java/com/example/trazabilidad/anadirA.kt
@@ -1,14 +1,12 @@
 package com.example.trazabilidad
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
-class MainActivity3 : AppCompatActivity() {
+class anadirA : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        setContentView(R.layout.`anadirA`)
 
 
     }
---
+++ app/src/main/java/com/example/trazabilidad/hcvacunas.kt
@@ -0,0 +1,11 @@
+package com.example.trazabilidad
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class hcvacunas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hcvacunas)
+    }
+}
